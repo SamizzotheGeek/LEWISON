5561b3f50eda1e98d79bb424b8b83377a97e15ef
SamizzotheGeek <njugunahmuiruri.nm@gmail.com>
2019-07-09 14:10:27 +0300

    updated menu button

--- js/lewison.js
+++ js/lewison.js
@@ -1,13 +1,16 @@
 var menuButton = document.getElementById('drop-menu');
 var menu = document.getElementById('menu');
+var close = document.getElementById("close");
+var bar = document.getElementById("bar");
 
 function showMenu(){
     if (menu.style.display === "none"){
         menu.style.display = "flex";
+        bar.className = "fas fa-times fa-2x";
     }else{
         menu.style.display = "none";
+        bar.className = "fas fa-bars fa-2x";
     }
 };
 
 menuButton.addEventListener("click", showMenu);
-//menuButton.addEventListener("click", hideMenu);
