5a81532854e454590e49749cd298eac362df0814
SamizzotheGeek <njugunahmuiruri.nm@gmail.com>
2019-08-01 09:16:54 +0300

    John Wachira

--- js/lewison.js
+++ js/lewison.js
@@ -1,6 +1,7 @@
 var menuButton = document.getElementById('drop-menu');
 var menu = document.getElementById('menu');
 var bar = document.getElementById("bar");
+var close = document.querySelector('.status')
 
 function showMenu(){
     if (menu.style.display === "none"){
@@ -13,3 +14,24 @@ function showMenu(){
 };
 
 menuButton.addEventListener("click", showMenu);
+
+pop = document.querySelector('#popup');
+window.onload = function hidePop(){
+   pop.style.display = "none";
+}
+
+window.setTimeout(function(){
+    pop.style.display = "block";
+}, 8000);
+
+window.setTimeout(function(){
+    if(pop.style.display = "block"){
+    pop.style.display = "none";
+    }
+}, 6000);
+
+function hide(){
+    if( pop.style.display = "block"){
+        pop.style.display = "none";
+    }
+}
